64238ae2af631e40994d91b7e77132646f23f1fe
VictorGomes <victorcrepaldigomes@gmail.com>
2024-11-29 14:08:58 -0300

    Melhorias

---
+++ script.js
@@ -0,0 +1,8 @@
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+    themeToggle.textContent = document.body.classList.contains("dark-theme")
+        ? "☀️"
+        : "🌙";
+});
